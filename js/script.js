2d12ca1215b4b3622d0eb407292f1b34d708e0d4
olegjump <olavrff@gmail.com>
2020-10-23 17:12:57 +0600

    make slider in the modal window

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const renderCard = (data) => {
    let i = 0;
 
    data.forEach(elem => {
-      const heroCard = document.createElement('div');
+      let heroCard = document.createElement('div');
       heroCard.classList.add('hero__card');
       heroCard.id = `${i}`;
       heroCard.innerHTML = `
@@ -34,37 +34,60 @@ const renderCard = (data) => {
       i++;
       heroCard.addEventListener('click', (event) => {
          heroPopup.style.display = 'flex';
-         renderCardBig(data, heroCard);
+         renderCardBig(data, heroCard.id);
+         let index = heroCard.id; // record current index of element
+
+         heroPopup.addEventListener('click', (event) => {
+            let target = event.target;
+         
+            // Close popup
+            if (target.closest('.hero__popup-close-btn') || !target.closest('.hero__card-big')) {
+               heroPopup.style.display = 'none';
+            }
+      
+            // #arrowLeft move slider to the left
+            if (target.closest('#arrowLeft')) {
+               if (index <= 0) {
+                  index = 0;
+               } else index--;
+
+               renderCardBig(data, index);
+            }
+
+            // #arrowRight move slider to the right
+            if (target.closest('#arrowRight')) {
+               if (index === data.length - 1) {
+                  index = data.length - 1;
+               } else index++;
+
+               renderCardBig(data, index);
+            }
+         });
       });
+
       heroCards.appendChild(heroCard);
    });
 }
 
-const renderCardBig = (data, heroCard) => {
+// render modal window with additional info of hero
+const renderCardBig = (data, index) => {
    heroPopup.innerHTML = `
       <div class="hero__card-big">
          <div class="hero__popup-close-btn"><img src="img/close.svg" alt="" /></div>
-         <div class="arrow arrow-left"></div>
-         <div class="arrow arrow-right"></div>
-         <div class="hero__photo-big"><img src="db_heroes/${data[heroCard.id]['photo']}" alt="" /></div>
+         <div class="arrow arrow-left" id="arrowLeft"></div>
+         <div class="arrow arrow-right" id="arrowRight"></div>
+         <div class="hero__photo-big"><img src="db_heroes/${data[index]['photo']}" alt="" /></div>
          <div class="hero__info">
-            <div class="hero__big-name">${data[heroCard.id]['name']}<div>
-            <div class="hero__big-realname">${data[heroCard.id]['realName']}<div>
-            <div class="hero__big-species">${data[heroCard.id]['species']}<div>
-            <div class="hero__big-actors">${data[heroCard.id]['actors']}<div>
-            <div class="hero__big-status">${data[heroCard.id]['status']} (${data[heroCard.id]['birthDay']} - ${data[heroCard.id]['deathDay']}<div>
-            <div class="hero__big-citizenship">${data[heroCard.id]['citizenship']}<div>
-            <div class="hero__big-movies">${data[heroCard.id]['movies']}<div>
+            <div class="hero__big-name">${data[index]['name']}<div>
+            <div class="hero__big-realname">${data[index]['realName']}<div>
+            <div class="hero__big-species">${data[index]['species']}<div>
+            <div class="hero__big-actors">${data[index]['actors']}<div>
+            <div class="hero__big-status">(${data[index]['birthDay']} - ${data[index]['deathDay']})<div>
+            <div class="hero__big-citizenship">${data[index]['citizenship']}<div>
+            <div class="hero__big-movies">${data[index]['movies']}<div>
          </div>
       </div>
    `;
-   heroPopup.addEventListener('click', (event) => {
-      let target = event.target;
-
-      if (target.closest('.hero__popup-close-btn') || !target.closest('.hero__card-big')) {
-         heroPopup.style.display = 'none';
-      }
-   });
 }
 
 
